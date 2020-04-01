e3e12c37fc49b3e11ef8bc646039499e37fede2e
Tjohnson456 <60521115+Tjohnson456@users.noreply.github.com>
2020-03-31 20:12:59 -0400

    Redesigned Page
    I want to use this as the page design.
    Animation added along with the new logo

---
+++ AttendWebsite(Tamikal)/phpfiles/instructorWP/populateTable.php
@@ -0,0 +1,88 @@
+<?php
+        session_start();
+        $courseNum = $_POST['courseID'];
+        $section = $_POST['section'];
+        
+        if( isset($courseNum) && isset($section) ){ 
+            //echo "connect";
+           
+            $host = "localhost";
+            $dbUsername = "root";
+            $dbPassword = "";
+            $dbName = "attenrecords";
+            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+
+            if(mysqli_connect_error()){ //checks if connection to server failed
+                die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_errno());
+            }
+
+            else{
+                $select = "SELECT CONCAT(s.Fname, ' ', s.Lname) AS Student_Name, s.Sex, s.Senority
+                        FROM student AS s
+                        WHERE s.Std_ID IN(SELECT Std_ID 
+                                            FROM enroll
+                                            WHERE Class_ID = ? AND Section = ?)";
+            
+                $stmt = $conn -> prepare($select);
+                $stmt -> bind_param("ss", $courseNum, $section);
+                $stmt -> execute();
+                $result = $stmt->get_result();
+
+                echo "<tr> 
+                        <th> Student Name </th> 
+                        <th> Gender </th> 
+                        <th> Senority </th> 
+                        </tr>";
+                
+                while($row = $result->fetch_assoc()){
+                    
+                    echo "<tr> <td>" . $row['Student_Name'] . "</td>" .
+                            "<td>" . $row['Sex'] . "</td>" .
+                            "<td>" . $row['Senority'] . "</td> </tr>";
+                    
+                }//end of loop
+               
+
+            }// end of else
+        }// end of if
+        
+
+    /*function populateTable($courseID, $course){
+            $host = "localhost";
+            $dbUsername = "root";
+            $dbPassword = "";
+            $dbName = "attenrecords";
+            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+            
+            if(mysqli_connect_error()){ //checks if connection to server failed
+                die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_errno());
+            }
+
+            else{
+                $select = "SELECT CONCAT(s.Fname, ' ', s.Lname) AS Student_Name, s.Sex, s.Senority
+                        FROM student AS s
+                        WHERE s.Std_ID IN(SELECT Std_ID 
+                                            FROM enroll
+                                            WHERE Class_ID = '?' AND Section = '?')";
+            
+                $stmt = $conn -> prepare($select);
+                $stmt -> bind_param("ss", $courseID, substr($course, -1));
+                $stmt -> execute();
+
+                echo "<tr> <th> Student Name </th> 
+                        <th> Gender </th> 
+                        <th> Senority </th> </tr>";
+                
+                while($row = $result->fetch_assoc()){
+                    
+                    echo "<tr> <td>" . $row['Student_Name'] . "</td>" .
+                            "<td>" . $row['Sex'] . "</td>" .
+                            "<td>" . $row['Senority'] . "</td> </tr>";
+                    
+                }//end of loop
+
+        }// end of else
+    }//end of function */
+
+?>
