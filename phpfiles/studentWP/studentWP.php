d3da5c48b61daa1b128336b02a92b6353029db6d
René Zambrana <rxzambrana@gmail.com>
2020-04-22 00:47:32 -0400

    Student button now shows attendance record of student with given ID. Also changed font colors on main page to enhance readability.

--- phpfiles/studentWP/studentWP.php
+++ phpfiles/studentWP/studentWP.php
@@ -9,7 +9,46 @@
 </head>
 
 <body>
+    <!--Gets the attendance records from the datbase for the student with the submitted ID. -->
     <div class = "banner"> </div>
+    <p id="greet"></p>
+    <p id="record"></p>
+    <?php
+      $num = $_SESSION['StnNum'];
+      $conn = mysqli_connect('localhost', 'root', '','attenrecords');
+      $sql = "SELECT * FROM attendence WHERE Std_ID=$num";
+      $sql2 = "SELECT Fname, Lname FROM student WHERE Std_ID=$num";
+      $result = $conn->query($sql);
+      $result2 = $conn->query($sql2);
+
+      $stdName = "Attendance record for ";
+      if($result2->num_rows > 0){
+        $row2 = $result2->fetch_assoc();
+        $stdName .= $row2["Fname"] . " " . $row2["Lname"] . ":";
+      }
+      else{
+        $stdName = "Invalid Student ID Entered";
+      }
+      $attendance = "Class ID | Section |  Std ID  | Log Time |  Log Date  | Attendance Status"."<br>";
+
+      if ($result->num_rows > 0) {
+          // output data of each row
+          while($row = $result->fetch_assoc()) {
+            if($row["Atten_status"] == "absent"){
+              $attendance .= $row["Class_ID"]. "   -    ". $row["Section"]. "   -   ". $row["Std_ID"]. " - ". $row["Log_time"]. " absent - ". $row["Log_date"].  " -  ".$row["Atten_status"]."<br>";
+            }
+            else{
+              $attendance .= $row["Class_ID"]. "   -    ". $row["Section"]. "   -   ". $row["Std_ID"]. " - ". $row["Log_time"]. "  - ". $row["Log_date"].  "  - ".$row["Atten_status"]."<br>";
+            }
+          }
+      } else {
+          $attendance .= "0 results";
+      }
+    ?>
+    <script>
+      document.getElementById("record").innerHTML = "<?php echo $attendance?>";
+      document.getElementById("greet").innerHTML = "<?php echo $stdName?>";
+    </script>
 
 </body>
 </html>
--- phpfiles/validateStudent.php
+++ phpfiles/validateStudent.php
@@ -6,7 +6,7 @@
 
     if(isset($_POST['StnNum'])){ // checks if input is null
         $number = '870'. $_POST['StnNum'];
-
+        $_SESSION['StnNum'] = $number;
         if(!preg_match("/870[0-9]{6}$/", $number)){ //checks if number matches this regex
             $error['number'] = "You have entered an invalid input";
 
@@ -17,15 +17,45 @@
             $host = "localhost";
             $dbUsername = "root";
             $dbPassword = "";
-            $dbName = "attenrecords";
-            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+            #$dbName = "attenrecords";
+            $conn = new mysqli($host, $dbUsername, $dbPassword/*, $dbName*/);
+
+
+            $sql = "CREATE DATABASE attenrecords";
+            mysqli_query($conn, $sql);
+
+            /*Stuff I added  to try and fill the "attenrecords" database using
+              the data from "insertData.sql".
+            */
+            $connection = mysqli_connect('localhost', 'root', '','attenrecords');
+            $filename = 'C:\xampp\htdocs\AttendWebsite\Database\record.sql';
+            $handle = fopen($filename,"r+");
+            $contents = fread($handle,filesize($filename));
+            $sql = explode(';',$contents);
+            foreach($sql as $query){
+              $result = mysqli_query($connection,$query);
+            }
+            fclose($handle);
+            $filename = 'C:\xampp\htdocs\AttendWebsite\Database\insertData.sql';
+            $handle = fopen($filename,"r+");
+            $contents = fread($handle,filesize($filename));
+            $sql = explode(';',$contents);
+            foreach($sql as $query){
+              $result = mysqli_query($connection,$query);
+            }
+            fclose($handle);
+
+            /*********************End of stuff I added.************************/
+/*Line 61 ("bind_param" line) gives an error that I could not fix, so I commented out this section.
+
+
 
             if(mysqli_connect_error()){ //checks if connection to server failed
                 die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_errno());
             }
 
             else{
-                $select = "SELECT Std_ID FROM student WHERE Std_ID = ?";
+                $select = "SELECT Std_ID FROM Student WHERE Std_ID = ?";
 
                 $stmt = $conn -> prepare($select);
                 $stmt -> bind_param("s", $number);
@@ -52,7 +82,8 @@
             else{// if no errors then redirect to page index
                 header('Location: studentWP/studentWP.php');
             }
-
+*/
+        header('Location: studentWP/studentWP.php');
 
         }// end of else
 
