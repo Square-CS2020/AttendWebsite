af0f987dce9c02f0b32e90a049fc54f178a3769c
Charles Samuel <csamuel1195@gmail.com>
2020-05-04 13:16:28 -0400

    moved some students php files.-charles

--- phpfiles/validateStudent.php
+++ phpfiles/validateStudent.php
@@ -2,39 +2,67 @@
 <?php //Checks if user input is valid
     session_start();
     $error = array('number' =>'', 'empty' => '');// stores any input errors
-    $studentNumber = '';
+    $number = '';
 
     if(isset($_POST['StnNum'])){ // checks if input is null
-        $studentNumber = '870'. $_POST['StnNum'];
-        
-        if(!preg_match("/870[0-9]{6}$/", $studentNumber)){ //checks if number matches this regex
+        $number = '870'. $_POST['StnNum'];
+        $_SESSION['StnNum'] = $number;
+        if(!preg_match("/870[0-9]{6}$/", $number)){ //checks if number matches this regex
             $error['number'] = "You have entered an invalid input";
-              
+
         }
 
         else{ // connects to the server and checks if ID is in the server
-            
+
             $host = "localhost";
             $dbUsername = "root";
             $dbPassword = "";
-            $dbName = "attenrecords";
-            $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+            #$dbName = "attenrecords";
+            $conn = new mysqli($host, $dbUsername, $dbPassword/*, $dbName*/);
+            //$conn = new mysqli($host, $dbUsername, $dbPassword/*, $dbName*/);
+
+
+            $sql = "CREATE DATABASE attenrecords";
+            mysqli_query($conn, $sql);
+
+            //Fills the "attenrecords" database using the data from "insertData.sql".
+
+            $connection = mysqli_connect('localhost', 'root', '','attenrecords');
+            $filename = 'C:\xampp\htdocs\AttendWebsite\Database\record.sql';
+            $handle = fopen($filename,"r+");
+            $contents = fread($handle,filesize($filename));
+            $sql = explode(';',$contents);
+            foreach($sql as $query){
+              $result = mysqli_query($connection,$query);
+            }
+            fclose($handle);
+            $filename = 'C:\xampp\htdocs\AttendWebsite\Database\insertData.sql';
+            $handle = fopen($filename,"r+");
+            $contents = fread($handle,filesize($filename));
+            $sql = explode(';',$contents);
+            foreach($sql as $query){
+              $result = mysqli_query($connection,$query);
+            }
+            fclose($handle);
+
+
+/*Line 61 ("bind_param" line) gives an error that I could not fix, so I commented out this section.
 
             if(mysqli_connect_error()){ //checks if connection to server failed
                 die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_errno());
             }
 
             else{
-                $select = "SELECT Std_ID FROM student WHERE Std_ID = ?";
-       
+                $select = "SELECT Std_ID FROM Student WHERE Std_ID = ?";
+
                 $stmt = $conn -> prepare($select);
-                $stmt -> bind_param("s", $studentNumber);
+                $stmt -> bind_param("s", $number);
                 $stmt -> execute();
                 $result = $stmt->get_result(); // get the mysqli result
-                $user = $result->fetch_assoc(); // fetch data 
-                
+                $user = $result->fetch_assoc(); // fetch data
+
                 if(empty($user)){
-                    $error['empty'] = "Could not find student record associated with this 870 number. 
+                    $error['empty'] = "Could not find student record associated with this 870 number.
                     Try again.";
                 }
 
@@ -51,12 +79,12 @@
 
             else{// if no errors then redirect to page index
                 header('Location: studentWP/studentWP.php');
-            }
+            }*/
 
-            
+        header('Location: studentWP/studentWP.php');
+
         }// end of else
-        
+
     }// end of POST check
 
-?> 
-
+?>
