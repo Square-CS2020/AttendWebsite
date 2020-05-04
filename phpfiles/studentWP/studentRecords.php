693c76a5e48b4e61b530a7c4b7d46adc0b5d46c5
Tjohnson456 <60521115+Tjohnson456@users.noreply.github.com>
2020-05-04 15:37:40 -0400

    Test for update Student Database

---
+++ phpfiles/studentWP/studentRecords.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbname = "attenrecords";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO student (std_ID, Fname, Lname, Sex, Senority)
+VALUES ('333555, John, Doe, Male, Sophmore')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
